edab4596c46d11ac36b19f5d32e57f8ef7f050a9
Averito <avenantmar@gmail.com>
2025-06-01 17:17:40 +0300

    feat: AppFoodCard.kt add description viewer

--- app/src/main/java/com/averito/firito/ui/shared/ui/app_food_card/AppFoodCard.kt
+++ app/src/main/java/com/averito/firito/ui/shared/ui/app_food_card/AppFoodCard.kt
@@ -2,30 +2,42 @@ package com.averito.firito.ui.shared.ui.app_food_card
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.averito.firito.core.models.food.FoodModel
 import com.averito.firito.data.models.food.FoodModelImpl
+import kotlinx.coroutines.launch
 import java.time.format.DateTimeFormatter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppFoodCard(
     foodModel: FoodModel,
@@ -33,11 +45,26 @@ fun AppFoodCard(
     onRemove: (() -> Unit)? = null,
     details: Boolean = false
 ) {
+    val sheetState = rememberModalBottomSheetState()
+    val coroutineScope = rememberCoroutineScope()
+
     val portion = foodModel.weightInGrams
     val totalCalories = foodModel.calories * portion / 100
     val totalProteins = foodModel.proteins * portion / 100f
     val totalFats = foodModel.fats * portion / 100f
     val totalCarbs = foodModel.carbs * portion / 100f
+
+    fun onSheetDismiss() {
+        coroutineScope.launch {
+            sheetState.hide()
+        }
+    }
+
+    fun openDescription() {
+        coroutineScope.launch {
+            sheetState.show()
+        }
+    }
 
     Card(
         modifier = Modifier
@@ -91,16 +118,30 @@ fun AppFoodCard(
                     }
                 }
 
-                if (onRemove != null) {
-                    IconButton(
-                        onClick = { onRemove.invoke() },
-                        modifier = Modifier.padding(start = 8.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = "Удалить",
-                            tint = MaterialTheme.colorScheme.error
-                        )
+                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    if (onRemove != null) {
+                        IconButton(
+                            onClick = { onRemove.invoke() },
+                            modifier = Modifier.padding(start = 8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Удалить",
+                                tint = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    }
+                    if (foodModel.description != null && details) {
+                        IconButton(
+                            onClick = { openDescription() },
+                            modifier = Modifier.padding(start = 8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Info,
+                                contentDescription = "Подробнее",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
                     }
                 }
             }
@@ -111,6 +152,15 @@ fun AppFoodCard(
                 style = MaterialTheme.typography.bodySmall,
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             )
+        }
+
+        if (sheetState.isVisible && foodModel.description != null) {
+            ModalBottomSheet(
+                onDismissRequest = { onSheetDismiss() },
+                sheetState = sheetState
+            ) {
+                Text(modifier = Modifier.padding(12.dp), text = foodModel.description!!)
+            }
         }
     }
 }
@@ -127,7 +177,7 @@ fun AppFoodCardPreview() {
             fats = 30f,
             proteins = 4f,
             weightInGrams = 125,
-            description = null
+            description = "asdasdsda"
         )
     )
 }
